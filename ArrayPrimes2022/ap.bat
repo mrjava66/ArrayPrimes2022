9a382c32a71bbe06d3c9e3a44205a85573b7030c
mrjava66 <jerry@lagrou.org>
2023-11-06 09:19:46 -0600

    better bat

--- ArrayPrimes2022/ap.bat
+++ ArrayPrimes2022/ap.bat
@@ -1,1 +1,3 @@
+del ap.*.log
+del ap.*.err
 ArrayPrimes2022.exe >"ap.%date:~10,4%_%date:~4,2%_%date:~7,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%.log" 2>"ap.%date:~10,4%_%date:~4,2%_%date:~7,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%.err"
