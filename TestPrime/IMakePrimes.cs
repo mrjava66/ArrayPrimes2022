27116ca9081f98d11e464f28233b61d13449e0d0
mrjava66 <jerry@lagrou.org>
2026-05-08 16:44:32 -0500

    Remove TestPrime

--- TestPrime/IMakePrimes.cs
+++
@@ -1,9 +0,0 @@
-namespace TestPrime;
-
-public interface IMakePrimes
-{
-    Dictionary<ulong, ulong> DictAllPrimes { get; }
-    int NumPrimes { get; }
-    ulong[] ArrayAllPrimes { get; }
-    void MakePrimesTask();
-}
